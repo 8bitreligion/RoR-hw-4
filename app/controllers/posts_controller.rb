f27c2980764b381fd58944dfb1c669ec753f63b0
Robert Lee Brackett III <robert.brackett3@gmail.com>
2015-01-05 19:24:38 -0500

    Update posts_controller.rb
    See comments in file, mostly just things to take note of as stuff would generally work the way you have it set up, but it depends what functionality you want.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,22 @@
 class PostsController < ApplicationController
 
 	def index
-		@posts = Post.all
-		@user = User.where(params[:user_id]).first
+		@posts = Post.all #this will show all posts for your app, if you want to only show a users posts...
+		# @oosts = Post.where(user_id: @user.id)
+		@user = User.where(params[:user_id]).first # this will give you an error if you try to go to /posts, but since you dont have routes for that it is ok
 	end
 
 	def show
-		@posts = Post.find(params[:id])
+		@posts = Post.find(params[:id]) #same as above, to scope to the user '/users/1/posts/1'...
+		# @user = User.where(params[:user_id]).first
+		# @post = @user.posts.find(params[:id]) # note that @post is singular for showing 1 post
 	end
 
 	def new
-		@post = Post.new
+		@post = Post.new # same idea as above
 		@user = User.where(params[:user_id]).first
+		
+		# @post = @user.posts.new # this will creata new post for a specific user if desired (not required)
 	end
 
 	def create
@@ -24,10 +29,22 @@ class PostsController < ApplicationController
 			flash[:alert] = "Your post could not be saved"
 			redirect_to user_posts_path
 		end
-			
-	 end
-
+	
+	
 	end
+	
+	####################
+	# missing methods
+	def edit
+	end
+	
+	def update
+	end
+	
+	def destroy
+	end
+	
+end
 
 
 
